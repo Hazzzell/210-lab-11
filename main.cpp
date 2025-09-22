de72f6e13e8489fabdad0f5ad6bc5bd334446bfd
Hazzzell <hazzzelHZ@gmail.com>
2025-09-21 19:57:44 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,58 +3,73 @@
 #include <iostream>
 using namespace std;
 
-const int NR_GRADES = 2, NR_STUDENTS = 3;
+const int NR_PLAYLISTS = 2, NR_SONGS = 3;
 
-struct Student {
+struct Song {
+    string title;
+    int duration; //in seconds
+};
+
+struct Playlist {
     string name;
-    int id;
-    int * grades;
+    int id; 
+    Song *songs; //dynamic array of songs
 
-    ~Student() {
-        if (grades) 
-            delete [] grades;
-        grades = nullptr;
+    ~Playlist() {
+        if (songs) 
+            delete [] songs;
+        songs = nullptr;
     }
 };
 
-void inputStudent(Student *);
-void displayStudent(Student *);
+// inputPlaylist() collects info for a playlist
+// arguments: Playlist* (Pointer to playlist object)
+// returns: NA
+void inputPlaylist(Playlist * sptr);
+// displayPlaylist() display the data of a playlist and its songs
+// arguments: Playlist* (Pointer to playlist object)
+// returns: NA
+void displayPlaylist(Playlist * sptr);
 
 int main() {
-    Student *roster = new Student[NR_STUDENTS];
+    Playlist *library = new Playlist[NR_PLAYLISTS];
     
-    for (int i = 0; i < NR_STUDENTS; i++)
-        inputStudent(&roster[i]);
+    for (int i = 0; i < NR_PLAYLISTS; i++)
+        inputPlaylist(&library[i]);
     
-    for (int i = 0; i < NR_STUDENTS; i++)
-        displayStudent(&roster[i]);
+    for (int i = 0; i < NR_PLAYLISTS; i++)
+        displayPlaylist(&library[i]);
     
     return 0;
 }
 
-void inputStudent(Student * sptr) {
-    static int nrStu = 1;
-    cout << "Input data for Student #" << nrStu << ":\n";
+void inputPlaylist(Playlist * sptr) {
+    static int nrPl = 1;
+    cout << "Input data for Playlist #" << nrPl << ":\n";
     cout << "Name: ";
     getline(cin, sptr->name);
     cout << "ID: ";
     cin >> sptr->id;
-    sptr -> grades = new int[NR_GRADES];
-    for (int i = 0; i < NR_GRADES; i++) {
-        cout << "Grade #" << i + 1 << ": ";
-        cin >> sptr->grades[i];
+    sptr -> songs = new Song[NR_SONGS];
+    for (int i = 0; i < NR_SONGS; i++) {
+        cout << "Song #" << i + 1 << "title: ";
+        cin.ignore();
+        getline(cin, sptr->songs[i].title);
+        cout << "Song #" << i+ 1 << "duration (seconds): ";
+        cin >> sptr->songs[i].duration;
     }
     cin.ignore();
     cout << endl << endl;
-    nrStu++;
+    nrPl++;
 }
 
-void displayStudent(Student * sptr) {
-    cout << "Student summary:\n";
+void displayPlaylist(Playlist * sptr) {
+    cout << "Playlist summary:\n";
     cout << "Name: " << sptr->name << endl;
     cout << "ID: " << sptr->id << endl;
-    for (int i = 0; i < NR_GRADES; i++)
-        cout << "Grade #" << i + 1 << ": " 
-             << sptr->grades[i] << endl;
+    for (int i = 0; i < NR_SONGS; i++)
+        cout << "Song #" << i + 1 << ": " 
+             << sptr->songs[i].title << " (" 
+             << sptr->songs[i].duration << "s)" << endl;
     cout << endl << endl;
 }
